e11d802a4d699c46c4fb2cbbb71ccd74731681cf
romanemaluf <eliasmaluf51@gmail.com>
2019-05-23 18:36:14 -0300

    #3 -> Se agrega al Main del proyecto. la funcion prototipo de Menu

--- src/main/AdivinaCuatro.java
+++ src/main/AdivinaCuatro.java
@@ -7,6 +7,8 @@
  */
 package main;
 
+import java.util.Scanner;
+
 
 
 /**
@@ -20,7 +22,42 @@ public class AdivinaCuatro {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       menu(); //Ejecuto el Menu del juego
+ 
+    }
+     public static int menu(){
+    
+        int opcion = 0;   
+        Scanner scn = new Scanner(System.in);
+            
+    while(opcion != 3) {  //Mientras la opcion no sea Salir sigo en el juego
+        System.out.println
+        ("Bienvenido Al Juego ***Adivina Cuatro Cifras***" +
+        "\n 1- Adivine un número generado por el ordenador"+
+        "\n 2- Ingrese un número para que el ordenador adivine"+
+        "\n 3- Salir");
         
+        opcion = scn.nextInt();
+        
+        switch(opcion){
+            case 1:
+                //Voy al Modelo Usuario
+                break;
+         
+            case 2:
+               //Voy al Modelo Ordenador
+                break;
+            
+            case 3:
+                System.out.println("Gracias por jugar");
+                return 1;
+            default:
+                System.out.println("Opcion Incorrecta. Vuelva a Intentar");
+        }   
+    }   
+
+    
+        return 0;
     }
     
    
